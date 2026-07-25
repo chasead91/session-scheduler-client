abdd889628e2a71d1aec5e2eb6d3fae8e9d892d1
Chase Aderhold <chase.aderhold@gmail.com>
2026-07-24 21:17:56 -0400

    set api base url to base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 // api/client.js
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'http://192.168.1.192:5000';
 
 export async function apiClient(endpoint, { body, headers: customHeaders, ...customConfig } = {}) {
   const token = localStorage.getItem('token');
